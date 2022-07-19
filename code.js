24a4a4d012e256641b47f4c2da77e2f1c7bf9590
lachi3 <54197094+lachi3@users.noreply.github.com>
2022-07-19 16:48:04 +1000

    1st Commit

---
+++ code.js
@@ -0,0 +1,54 @@
+onEvent("continuelanding", "click", function( ) {
+	setScreen("Main");
+});
+onEvent("mainback", "click", function( ) {
+  setScreen("Landing");
+});
+onEvent("helpselect", "click", function( ) {
+  setScreen("help");
+});
+onEvent("send", "click", function( ) {
+  setScreen("Thanks");
+});
+onEvent("thanksback", "click", function( ) {
+  setScreen("Main");
+});
+onEvent("backhelp", "click", function( ) {
+  setScreen("Main");
+});
+onEvent("reportselect", "click", function( ) {
+	setScreen("Report");
+});
+onEvent("reportsend", "click", function( ) {
+	setScreen("Thanks");
+});
+onEvent("reportback", "click", function( ) {
+	setScreen("Main");
+});
+onEvent("suggestionsend", "click", function( ) {
+	setScreen("Thanks");
+});
+onEvent("suggestionsback", "click", function( ) {
+	setScreen("Main");
+});
+onEvent("suggestselect", "click", function( ) {
+	setScreen("Suggestions");
+});
+onEvent("button5", "click", function( ) {
+	setScreen("Thanks");
+});
+onEvent("button6", "click", function( ) {
+	setScreen("Main");
+});
+onEvent("tipselect", "click", function( ) {
+	setScreen("Tips");
+});
+onEvent("feedbackselect", "click", function( ) {
+	setScreen("Feedback");
+});
+onEvent("feebacksend", "click", function( ) {
+	setScreen("Thanks");
+});
+onEvent("feedbackback", "click", function( ) {
+	setScreen("Main");
+});
